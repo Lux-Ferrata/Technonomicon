5f1eb050715ec267af2b671ed0af3d8a97961bea
xin <git@ironshark.org>
2026-06-27 19:37:58 -0400

    auto: update desktop.nix

--- Modules/Core/desktop.nix
+++ Modules/Core/desktop.nix
@@ -95,6 +95,7 @@
       QT_AUTO_SCREEN_SCALE_FACTOR = "0";
       QT_SCALE_FACTOR = "1";
       GDK_SCALE = "1";
+      GDK_BACKEND = "wayland,x11";
       ANKI_WAYLAND = "1";
       NIXPKGS_ALLOW_UNFREE = "1";
     };
