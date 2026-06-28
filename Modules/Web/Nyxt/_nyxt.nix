8d4358cc023ab349ff59190995d3e155cf27093b
xin <git@ironshark.org>
2026-06-28 07:29:43 -0400

    auto: update _nyxt.nix

--- Modules/Web/Nyxt/_nyxt.nix
+++ Modules/Web/Nyxt/_nyxt.nix
@@ -4,7 +4,7 @@ let
   version = "4.0.0";
   archive = pkgs.fetchurl {
     url = "https://github.com/atlas-engineer/nyxt/releases/download/${version}/Linux-Nyxt-x86_64.tar.gz";
-    sha256 = "0zc4pha3hmlaxwa3v9prd1jfiwdv2pblclfjjmk5ks16vx9df3ma";
+    hash = "sha256-v+x6K5svLA3L+IjEdTjmJEf3hvgwhwrvqAcelpY1ScQ=";
   };
   appimage = pkgs.runCommand "nyxt-${version}.AppImage" { } ''
     tar -xf ${archive}
