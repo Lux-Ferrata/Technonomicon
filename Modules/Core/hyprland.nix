1b2dcc835e27515771047cdc5d750645a6484446
xin <git@ironshark.org>
2026-06-30 15:37:23 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -1,15 +1,6 @@
 { inputs, ... }: {
   flake.nixosModules.Tn-hyprland = { pkgs, ... }:
   let
-    smartClose = pkgs.writeShellScript "smart-close" ''
-      if ${pkgs.hyprland}/bin/hyprctl activewindow -j \
-          | ${pkgs.jq}/bin/jq -e '.class == "obsidian"' > /dev/null 2>&1; then
-        ${pkgs.hyprland}/bin/hyprctl dispatch movetoworkspacesilent special:obsidian
-      else
-        ${pkgs.hyprland}/bin/hyprctl dispatch closewindow activewindow
-      fi
-    '';
-
   in {
 
     programs.hyprland.enable = true;
