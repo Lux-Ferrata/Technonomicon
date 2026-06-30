eb253d9651b529ba877f0faa35836230139fa73b
xin <git@ironshark.org>
2026-06-30 16:00:26 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -143,6 +143,7 @@
           hl.bind(mainMod .. " + F",         hl.dsp.exec_cmd("ghostty -e yazi $HOME"))
           hl.bind(mainMod .. " + SHIFT + F", hl.dsp.exec_cmd("nemo"))
           hl.bind(mainMod .. " + N",         hl.dsp.exec_cmd("${activateObsidian}"))
+          hl.bind(mainMod .. " + 0",         hl.dsp.exec_cmd("ghostty --class=grimoire-inbox -e nvim $HOME/Grimoire/Inbox.md"))
 hl.bind(mainMod .. " + left",  hl.dsp.focus({ direction = "left"  }))
           hl.bind(mainMod .. " + down",  hl.dsp.focus({ direction = "down"  }))
           hl.bind(mainMod .. " + up",    hl.dsp.focus({ direction = "up"    }))
