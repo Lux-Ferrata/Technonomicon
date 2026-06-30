8cb999eda6962ea47ac8b6d9c0a1893a76e349da
xin <git@ironshark.org>
2026-06-30 13:17:11 -0400

    auto: update mind.nix

--- Modules/Knowledge/mind.nix
+++ Modules/Knowledge/mind.nix
@@ -1,13 +1,7 @@
 { inputs, ... }: {
   flake.nixosModules.Tn-mind = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
-      (obsidian.overrideAttrs (oldAttrs: {
-        postInstall = (oldAttrs.postInstall or "") + ''
-          desktop=$out/share/applications/obsidian.desktop
-          [ -L "$desktop" ] && cp --remove-destination "$(readlink -f "$desktop")" "$desktop"
-          echo "StartupWMClass=electron" >> "$desktop"
-        '';
-      }))
+      obsidian
       taskwarrior3
       timewarrior
     ];
