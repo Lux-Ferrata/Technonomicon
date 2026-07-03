004dc59cf89e07bc36a90ca2dcd05289673de556
xin <git@ironshark.org>
2026-07-03 18:56:33 -0400

    auto: update neovim.nix

--- Modules/Core/neovim.nix
+++ Modules/Core/neovim.nix
@@ -262,7 +262,11 @@
           autocmds = ''
             vim.api.nvim_create_autocmd("BufWritePre", {
               pattern = "*",
-              callback = function() require("mini.trailspace").trim() end,
+              callback = function()
+                local pos = vim.api.nvim_win_get_cursor(0)
+                vim.cmd([[%s/\s\+$//e]])
+                vim.api.nvim_win_set_cursor(0, pos)
+              end,
             })
 
             vim.api.nvim_create_autocmd("FileType", {
