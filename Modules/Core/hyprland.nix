108289703105c6ec38bffe2c59eb06657cfb20f0
xin <git@ironshark.org>
2026-07-03 18:19:45 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -150,7 +150,8 @@
           hl.config({
             input = {
               kb_layout    = "us",
-              follow_mouse = 0,
+              follow_mouse              = 0,
+              float_switch_override_focus = 0,
               touchpad     = { natural_scroll = true, disable_while_typing = true, drag_lock = false },
             },
             general = {
