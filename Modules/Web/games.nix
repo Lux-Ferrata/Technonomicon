59822786fdbf1eeb293f93a7dbc37ca926543f1a
xin <git@ironshark.org>
2026-07-02 20:24:42 -0400

    auto: update games.nix

--- Modules/Web/games.nix
+++ Modules/Web/games.nix
@@ -1,11 +1,14 @@
 { inputs, ... }: {
-  flake.nixosModules.Tn-games = { pkgs, ... }: {
-
+  flake.nixosModules.Tn-games = { pkgs, ... }:
+    let
+      weiqi-hub = pkgs.callPackage (import ./WeiqiHub/_weiqi-hub.nix) {};
+    in {
 
     environment.systemPackages = with pkgs; [
       xivlauncher
       hyperspeedcube
       exercism
+      weiqi-hub
     ];
   };
 }
