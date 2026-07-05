9974988d430e1c2c627c4e141d622b9bfdf33865
xin <git@ironshark.org>
2026-07-04 21:13:39 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -164,11 +164,12 @@
           })
 
           hl.window_rule({
-            name   = "habitica-float",
-            match  = { class = "brave-habitica" },
-            float  = true,
-            size   = "1200 800",
-            center = true,
+            name      = "habitica-special",
+            match     = { class = "brave-habitica" },
+            workspace = "special:habitica",
+            float     = true,
+            size      = "1200 800",
+            center    = true,
           })
 
           hl.on("hyprland.start", function()
