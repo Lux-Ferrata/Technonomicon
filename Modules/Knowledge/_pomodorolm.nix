4b17b973c7b9865019a2ef5aef0483c4f59c729a
xin <git@ironshark.org>
2026-07-06 16:53:55 -0400

    auto: update _pomodorolm.nix

--- Modules/Knowledge/_pomodorolm.nix
+++ Modules/Knowledge/_pomodorolm.nix
@@ -1,10 +1,21 @@
 { pkgs }:
 
-pkgs.appimageTools.wrapType2 {
-  pname = "pomodorolm";
-  version = "0.9.1";
-  src = pkgs.fetchurl {
-    url = "https://github.com/vjousse/pomodorolm/releases/download/app-v0.9.1/pomodorolm_0.9.1_amd64.AppImage";
-    hash = "sha256-kZYJnmFZFHVgX4IBWg6cyjuHeQWtUkLLnxyaeWIHZAQ=";
+let
+  appimage = pkgs.appimageTools.wrapType2 {
+    pname = "pomodorolm";
+    version = "0.9.1";
+    src = pkgs.fetchurl {
+      url = "https://github.com/vjousse/pomodorolm/releases/download/app-v0.9.1/pomodorolm_0.9.1_amd64.AppImage";
+      hash = "sha256-kZYJnmFZFHVgX4IBWg6cyjuHeQWtUkLLnxyaeWIHZAQ=";
+    };
   };
+in
+pkgs.symlinkJoin {
+  name = "pomodorolm";
+  paths = [ appimage ];
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = ''
+    wrapProgram $out/bin/pomodorolm \
+      --set WEBKIT_DISABLE_COMPOSITING_MODE 1
+  '';
 }
