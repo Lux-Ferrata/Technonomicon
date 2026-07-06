c614c38e44fad97c0a17b3d3510278083dfcf4da
xin <git@ironshark.org>
2026-07-06 18:10:32 -0400

    auto: update mind.nix

--- Modules/Knowledge/mind.nix
+++ Modules/Knowledge/mind.nix
@@ -1,13 +1,18 @@
 { inputs, ... }: {
-  flake.nixosModules.Tn-mind = { pkgs, ... }:
-  let
-    pomodorolm = pkgs.callPackage ./_pomodorolm.nix {};
-  in {
+  flake.nixosModules.Tn-mind = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
       obsidian
       taskwarrior3
       timewarrior
-      pomodorolm
+
+      (pkgs.makeDesktopItem {
+        name = "pomofocus";
+        desktopName = "Pomofocus";
+        exec = "${pkgs.brave}/bin/brave --app=https://pomofocus.io --start-maximized";
+        icon = "${pkgs.fetchurl { name = "timer.svg"; url = "https://api.iconify.design/mdi:timer.svg"; sha256 = "sha256-TMBdlXz0OmiiFTaKJkeLpPeAsnFbqP1AGN4wiufqoVk="; }}";
+        terminal = false;
+        categories = [ "Application" "Network" ];
+      })
     ];
 
     xdg.mime.defaultApplications = {
