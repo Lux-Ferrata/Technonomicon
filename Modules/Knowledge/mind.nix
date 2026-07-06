cdfbe0e310886e48458ba0c368050120efe4c1df
xin <git@ironshark.org>
2026-07-06 16:16:56 -0400

    auto: update mind.nix

--- Modules/Knowledge/mind.nix
+++ Modules/Knowledge/mind.nix
@@ -10,9 +10,6 @@
       tomatych
     ];
 
-    sops.secrets.habitica-api-token = { owner = "xin"; };
-    sops.secrets.habitica-user-id   = { owner = "xin"; };
-    sops.secrets.habitica-task-id   = { owner = "xin"; };
 
     xdg.mime.defaultApplications = {
       "x-scheme-handler/obsidian" = "obsidian.desktop";
