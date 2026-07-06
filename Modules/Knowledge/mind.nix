815ffb332ccae364cfa7ab2ac95fead02f64bf9e
xin <git@ironshark.org>
2026-07-06 16:12:34 -0400

    auto: update mind.nix

--- Modules/Knowledge/mind.nix
+++ Modules/Knowledge/mind.nix
@@ -1,10 +1,18 @@
 { inputs, ... }: {
-  flake.nixosModules.Tn-mind = { pkgs, ... }: {
+  flake.nixosModules.Tn-mind = { pkgs, ... }:
+  let
+    tomatych = pkgs.callPackage ./_tomatych.nix {};
+  in {
     environment.systemPackages = with pkgs; [
       obsidian
       taskwarrior3
       timewarrior
+      tomatych
     ];
+
+    sops.secrets.habitica-api-token = { owner = "xin"; };
+    sops.secrets.habitica-user-id   = { owner = "xin"; };
+    sops.secrets.habitica-task-id   = { owner = "xin"; };
 
     xdg.mime.defaultApplications = {
       "x-scheme-handler/obsidian" = "obsidian.desktop";
