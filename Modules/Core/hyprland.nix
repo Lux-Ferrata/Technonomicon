8bc5eade16856dbb090be7d623c202d4e684b753
xin <git@ironshark.org>
2026-07-06 21:46:34 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -247,7 +247,6 @@
 
           hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
           hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
-          hl.bind(mainMod .. " + mouse:274", hl.dsp.exec_cmd("/etc/scripts/scrollshot.sh"))
 
         '';
 
