4adde1883cc0a2e81945fc426ed06b08db1a560c
xin <git@ironshark.org>
2026-07-09 07:46:37 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -425,7 +425,7 @@
 
           hl.on("hyprland.start", function()
             hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE GDK_BACKEND")
-            hl.exec_cmd("$HOME/.local/share/tn/bin/tn-group-daemon &")
+            hl.exec_cmd("$HOME/.local/share/tn/bin/tn-group-daemon")
             hl.exec_cmd("quickshell")
             hl.exec_cmd("udiskie --tray")
             hl.exec_cmd("blueman-applet")
