3cbe15425fe15bb9ec6b99f1fbb2f90ede3ac0b6
xin <git@ironshark.org>
2026-07-09 08:22:13 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -412,7 +412,7 @@
             hl.exec_cmd("hyprsunset")
             hl.exec_cmd("systemctl --user start hyprpolkitagent")
             hl.exec_cmd("obsidian")
-            hl.exec_cmd("plover")
+            hl.exec_cmd("env QT_QPA_PLATFORM=xcb plover")
             hl.exec_cmd("[workspace 9 silent] flatpak run com.discordapp.Discord")
           end)
 
