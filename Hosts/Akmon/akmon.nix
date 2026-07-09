6e505c68409fef0dbaa1fd2885a0a4a811ebeeb5
xin <git@ironshark.org>
2026-07-08 23:24:11 -0400

    auto: update akmon.nix

--- Hosts/Akmon/akmon.nix
+++ Hosts/Akmon/akmon.nix
@@ -81,7 +81,7 @@
 
         sops.secrets.xin-password.neededForUsers = true;
         sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-        sops.defaultSopsFile = "${inputs.self}/_secrets.yaml";
+        sops.defaultSopsFile = ../../_secrets.yaml;
         sops.defaultSopsFormat = "yaml";
 
         networking.hostName = "Akmon";
