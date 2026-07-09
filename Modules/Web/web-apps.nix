eea1897f8ecda94f43ba0f1748ab4c6dfe0e9100
xin <git@ironshark.org>
2026-07-09 08:24:32 -0400

    auto: update web-apps.nix

--- Modules/Web/web-apps.nix
+++ Modules/Web/web-apps.nix
@@ -1,25 +1,25 @@
 { inputs, ... }: {
   flake.nixosModules.Tn-web-apps = { pkgs, config, ... }:
     let
-      icon = url: sha256: pkgs.fetchurl { inherit url sha256; };
+      icon = name: url: sha256: pkgs.fetchurl { inherit name url sha256; };
       icons = {
-        khanAcademy  = icon "https://api.iconify.design/simple-icons:khanacademy.svg?width=128&height=128"  "06azkd71q5f9ibc20l33kvn8vx8zkx4b2z3f9c26zd6kk45m69lc";
-        odinProject  = icon "https://api.iconify.design/simple-icons:theodinproject.svg?width=128&height=128" "1rqwdrn08974kamd7p044hl0slg8mdiaxcldpmj5dpmy93kpwccz";
-        youtube      = icon "https://api.iconify.design/logos:youtube-icon.svg?width=128&height=128"          "000v6w4apb6dkwhk4c0qwsx06qnax6hs4nyr16jp9x0f6b15qr2p";
-        gemini       = icon "https://api.iconify.design/logos:google-gemini.svg?width=128&height=128"         "02jq5rynxlca6h4pxp0n3ybzlw21yzgc62jg5ghdg11wwddrwxi6";
-        stenoJig     = icon "https://api.iconify.design/tabler:keyboard.svg?width=128&height=128"             "062f03p0fnp1biyig9c3rlalzk37j52qf297484hjmrsfwmg51a6";
-        typeyType    = icon "https://didoesdigital.com/typey-type/favicon-192x192.png"                        "0736jdsqnj0pj8m1gpqpqrhwgbf8diz6zwn2qp8xcd7rm8f3s54j";
+        khanAcademy  = icon "khanacademy.svg"    "https://api.iconify.design/simple-icons:khanacademy.svg?width=128&height=128"     "06azkd71q5f9ibc20l33kvn8vx8zkx4b2z3f9c26zd6kk45m69lc";
+        odinProject  = icon "theodinproject.svg" "https://api.iconify.design/simple-icons:theodinproject.svg?width=128&height=128"   "1rqwdrn08974kamd7p044hl0slg8mdiaxcldpmj5dpmy93kpwccz";
+        youtube      = icon "youtube-icon.svg"   "https://api.iconify.design/logos:youtube-icon.svg?width=128&height=128"            "000v6w4apb6dkwhk4c0qwsx06qnax6hs4nyr16jp9x0f6b15qr2p";
+        gemini       = icon "google-gemini.svg"  "https://api.iconify.design/logos:google-gemini.svg?width=128&height=128"           "02jq5rynxlca6h4pxp0n3ybzlw21yzgc62jg5ghdg11wwddrwxi6";
+        stenoJig     = icon "keyboard.svg"       "https://api.iconify.design/tabler:keyboard.svg?width=128&height=128"               "062f03p0fnp1biyig9c3rlalzk37j52qf297484hjmrsfwmg51a6";
+        typeyType    = icon "typey-type.png"     "https://didoesdigital.com/typey-type/favicon-192x192.png"                          "0736jdsqnj0pj8m1gpqpqrhwgbf8diz6zwn2qp8xcd7rm8f3s54j";
         gmail        = pkgs.fetchurl { name = "gmail.svg"; sha256 = "01gvhxl2wxjmfj5fhdmr3l12ydlmkiqna5snpgk1nd4wjgzrs4ny"; url = "https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg"; };
-        calendar     = icon "https://api.iconify.design/logos:google-calendar.svg?width=128&height=128"       "16m2wvm5rm5gqfv6zgrwbqkhb4jinbk1p5l3mdjdsmxrqcm0grj5";
-        toggl        = icon "https://api.iconify.design/simple-icons:toggl.svg?width=128&height=128"          "1ljdq4vgzzxiflpjn4ag6yyzlh27fx2ljh41lyyw4zyanlq5ygwd";
-        syncthing    = icon "https://api.iconify.design/simple-icons:syncthing.svg?width=128&height=128"      "1qkwh9029zslazknaacvpjvzs2dgblizf9k89pc8rhjr2hbzq80f";
-        exercism     = icon "https://api.iconify.design/simple-icons:exercism.svg?width=128&height=128"       "1hcb0ny4yc45g50srmzzni1pxlkbbivghkknrzfsgpp1dsy1gpw7";
-        bitwarden    = icon "https://api.iconify.design/simple-icons:bitwarden.svg?width=128&height=128"      "1d8djqdz0zar6p7nm2pvfkqq1xkd8fd0sx2q3wqmh72qwjbhpwx5";
-        habitica     = icon "https://api.iconify.design/game-icons:dragon-head.svg?width=128&height=128"      "0k28xw0vhinaa1320nvgfmadlp9mc7svvs02isrj1m965ygb9bjg";
-        amazon       = pkgs.fetchurl { name = "amazon.svg"; sha256 = "1xc3qdzp1gzhg8py1j9mzbj0ik9g66l35rfrby7d5pb5wi881kb9"; url = "https://api.iconify.design/simple-icons:amazon.svg?width=128&height=128"; };
+        calendar     = icon "google-calendar.svg"  "https://api.iconify.design/logos:google-calendar.svg?width=128&height=128"       "16m2wvm5rm5gqfv6zgrwbqkhb4jinbk1p5l3mdjdsmxrqcm0grj5";
+        toggl        = icon "toggl.svg"            "https://api.iconify.design/simple-icons:toggl.svg?width=128&height=128"          "1ljdq4vgzzxiflpjn4ag6yyzlh27fx2ljh41lyyw4zyanlq5ygwd";
+        syncthing    = icon "syncthing.svg"        "https://api.iconify.design/simple-icons:syncthing.svg?width=128&height=128"      "1qkwh9029zslazknaacvpjvzs2dgblizf9k89pc8rhjr2hbzq80f";
+        exercism     = icon "exercism.svg"         "https://api.iconify.design/simple-icons:exercism.svg?width=128&height=128"       "1hcb0ny4yc45g50srmzzni1pxlkbbivghkknrzfsgpp1dsy1gpw7";
+        bitwarden    = icon "bitwarden.svg"        "https://api.iconify.design/simple-icons:bitwarden.svg?width=128&height=128"      "1d8djqdz0zar6p7nm2pvfkqq1xkd8fd0sx2q3wqmh72qwjbhpwx5";
+        habitica     = icon "dragon-head.svg"      "https://api.iconify.design/game-icons:dragon-head.svg?width=128&height=128"      "0k28xw0vhinaa1320nvgfmadlp9mc7svvs02isrj1m965ygb9bjg";
+        amazon       = pkgs.fetchurl { name = "amazon.svg";  sha256 = "1xc3qdzp1gzhg8py1j9mzbj0ik9g66l35rfrby7d5pb5wi881kb9"; url = "https://api.iconify.design/simple-icons:amazon.svg?width=128&height=128"; };
         weather      = pkgs.fetchurl { name = "weather.svg"; sha256 = "1kj75df6j02phrbb6ziqihvc5rhxfc5p1z0gakjzqm72g8qvwn4d"; url = "https://api.iconify.design/simple-icons:theweatherchannel.svg?width=128&height=128"; };
-        pima         = pkgs.fetchurl { name = "pima.svg"; sha256 = "0d0kw117lp3mna02zl61pzqa9khj9p6sgdk1pmd6h8hy7yaviksz"; url = "https://api.iconify.design/mdi:school.svg?width=128&height=128"; };
-        ogs          = pkgs.fetchurl { name = "ogs.svg"; sha256 = "1v6fgy5d18fkaz5sv9h71kq91xmw3s1mpcyw2g2lrbd8v041cz6d"; url = "https://api.iconify.design/simple-icons:go.svg?width=128&height=128"; };
+        pima         = pkgs.fetchurl { name = "pima.svg";    sha256 = "0d0kw117lp3mna02zl61pzqa9khj9p6sgdk1pmd6h8hy7yaviksz"; url = "https://api.iconify.design/mdi:school.svg?width=128&height=128"; };
+        ogs          = pkgs.fetchurl { name = "ogs.svg";     sha256 = "1v6fgy5d18fkaz5sv9h71kq91xmw3s1mpcyw2g2lrbd8v041cz6d"; url = "https://api.iconify.design/simple-icons:go.svg?width=128&height=128"; };
         tsumego      = pkgs.fetchurl { name = "tsumego.svg"; sha256 = "0hcldvigsh69fp1yvgjsmchxk9fj38rzfczczw60pf0h0373c1xa"; url = "https://api.iconify.design/game-icons:stone-pile.svg?width=128&height=128"; };
         kifubara     = pkgs.fetchurl { name = "kifubara.svg"; sha256 = "0xh0lqfyvyn1bfa4k5515dq0p1z57zii66154xfa5f3lnqp58g9m"; url = "https://api.iconify.design/game-icons:abstract-119.svg?width=128&height=128"; };
       };
