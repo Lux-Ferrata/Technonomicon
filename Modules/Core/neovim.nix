0464aea548ba2e9e9d4e13705aa619de7488edb9
xin <git@ironshark.org>
2026-07-08 21:12:03 -0400

    auto: update neovim.nix

--- Modules/Core/neovim.nix
+++ Modules/Core/neovim.nix
@@ -197,7 +197,6 @@
           lang.typescript       = { enable = true; installDependencies = false; };
           lang.tex              = { enable = true; };
           coding.luasnip        = { enable = true; };
-          ui.render-markdown    = { enable = true; };
         };
 
         plugins = {
