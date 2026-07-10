a517cbb21427be47459c562ac0d54aba57d756e5
xin <git@ironshark.org>
2026-07-09 22:37:07 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -75,12 +75,18 @@
     tnShowKeybindings = pkgs.writeShellScript "tn-show-keybindings"
       (builtins.readFile ../../bin/tn-show-keybindings);
 
+    cleanWin = ''
+      def clean_title: gsub("^[a-z][a-z-]* \\| [\\u2800-\\u28ff] "; "");
+      def clean_class: gsub("^([a-z0-9]+\\.)+"; "") | split("-") | .[0] | (.[0:1] | ascii_upcase) + .[1:];
+    '';
+
     winPicker = pkgs.writeShellScript "tn-win-picker" ''
       CHOICE=$(${hyprlandPkg}/bin/hyprctl clients -j | \
         ${pkgs.jq}/bin/jq -r '
+          ${cleanWin}
           sort_by(if .focusHistoryID == 0 then 999999 else .focusHistoryID end) |
-          .[] | [.title, .class, (.workspace.id | tostring), .address] | @tsv' | \
-        awk -F'\t' '{ printf "%-50s %-25s ws:%-2s  %s\n", $1, $2, $3, $4 }' | \
+          .[] | [(.title | clean_title), (.class | clean_class), (.workspace.id | tostring), .address] | @tsv' | \
+        awk -F'\t' '{ printf "%-50s %-12s ws:%-2s  %s\n", $1, $2, $3, $4 }' | \
         ${pkgs.wofi}/bin/wofi --dmenu -p "window")
       ADDR=$(echo "$CHOICE" | awk '{ print $NF }')
       ${hyprlandPkg}/bin/hyprctl eval "hl.dispatch(hl.dsp.focus({window='address:$ADDR'}))"
@@ -90,10 +96,11 @@
       WS_ID=$(${hyprlandPkg}/bin/hyprctl activeworkspace -j | ${pkgs.jq}/bin/jq '.id')
       CHOICE=$(${hyprlandPkg}/bin/hyprctl clients -j | \
         ${pkgs.jq}/bin/jq -r --argjson ws "$WS_ID" '
+          ${cleanWin}
           map(select(.workspace.id == $ws)) |
           sort_by(if .focusHistoryID == 0 then 999999 else .focusHistoryID end) |
-          .[] | [.title, .class, .address] | @tsv' | \
-        awk -F'\t' '{ printf "%-50s %-25s  %s\n", $1, $2, $3 }' | \
+          .[] | [(.title | clean_title), (.class | clean_class), .address] | @tsv' | \
+        awk -F'\t' '{ printf "%-50s %-12s  %s\n", $1, $2, $3 }' | \
         ${pkgs.wofi}/bin/wofi --dmenu -p "workspace window")
       ADDR=$(echo "$CHOICE" | awk '{ print $NF }')
       ${hyprlandPkg}/bin/hyprctl eval "hl.dispatch(hl.dsp.focus({window='address:$ADDR'}))"
