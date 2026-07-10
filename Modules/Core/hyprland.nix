e8cccdcd757f7f3d7bd03360fea5b619bcb9f1cf
xin <git@ironshark.org>
2026-07-09 21:43:38 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -555,7 +555,10 @@
 
           hl.bind("Print",           hl.dsp.exec_cmd("grim -g \"$(slurp)\" - | wl-copy"))
           hl.bind(mainMod .. " + Y", hl.dsp.exec_cmd("grim -g \"$(slurp)\" - | wl-copy"))
-          hl.bind(mainMod .. " + M", hl.dsp.layout("colresize +conf"))
+          hl.bind(mainMod .. " + M",         hl.dsp.layout("colresize +conf"))
+          hl.bind(mainMod .. " + SHIFT + M", hl.dsp.layout("togglemonocle"))
+          hl.bind(mainMod .. " + W",         hl.dsp.exec_cmd("${winPicker}"))
+          hl.bind(mainMod .. " + SHIFT + W", hl.dsp.exec_cmd("${winPull}"))
 
           hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),             { mouse = true })
           hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(),           { mouse = true })
