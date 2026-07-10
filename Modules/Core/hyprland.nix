2538de5e1bf2f955fa633c6014323251c25889df
xin <git@ironshark.org>
2026-07-09 22:05:23 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -449,7 +449,7 @@
           end)
 
           local monocleInitialized = false
-          hl.on("hyprland.window.open", function()
+          hl.on("window.open", function()
             if not monocleInitialized then
               monocleInitialized = true
               hl.dispatch(hl.dsp.layout("setmode monocle"))
