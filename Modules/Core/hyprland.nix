bbcb224cb9502cf8d6ca39dbc08c66a7621e511c
xin <git@ironshark.org>
2026-07-21 19:40:27 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -529,6 +529,7 @@
             hl.exec_cmd("obsidian")
             -- hl.exec_cmd("env QT_QPA_PLATFORM=xcb plover")
             hl.exec_cmd("[workspace 9 silent] flatpak run com.discordapp.Discord")
+            hl.exec_cmd("[workspace 9 silent] ${pkgs.brave}/bin/brave --app=https://habitica.com --start-maximized")
           end)
 
           hl.config({
