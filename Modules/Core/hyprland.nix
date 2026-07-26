b5c4c06b9107e84a16720a6852846d4de550e453
xin <git@ironshark.org>
2026-07-26 19:56:26 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -578,6 +578,7 @@
           hl.bind(mainMod .. " + T",          hl.dsp.exec_cmd(terminal))
           hl.bind(mainMod .. " + S",          hl.dsp.exec_cmd("brave"))
           hl.bind(mainMod .. " + D",          hl.dsp.window.close())
+          hl.bind(mainMod .. " + SHIFT + D",  hl.dsp.exec_cmd("${pkillMenu}"))
           hl.bind(mainMod .. " + Q",          hl.dsp.exec_cmd("hyprlock"))
           hl.bind(mainMod .. " + SHIFT + Q",  hl.dsp.exec_cmd("${pkgs.systemd}/bin/systemd-run --user --no-block --collect /etc/scripts/clean-power-off.sh"))
           hl.bind(mainMod .. " + SHIFT + E",  hl.dsp.exit())
