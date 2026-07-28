ed5ce68157efe7e74efdc81fa97a10ffd7ae80b2
xin <git@ironshark.org>
2026-07-28 04:50:07 -0400

    auto: update hyprland.nix

--- Modules/Core/hyprland.nix
+++ Modules/Core/hyprland.nix
@@ -494,7 +494,7 @@
             hl.exec_cmd("clipse -listen")
             hl.exec_cmd("hyprsunset")
             hl.exec_cmd("systemctl --user start hyprpolkitagent")
-            hl.exec_cmd("obsidian")
+            hl.exec_cmd("[workspace 8 silent] obsidian")
             -- hl.exec_cmd("env QT_QPA_PLATFORM=xcb plover")
             hl.exec_cmd("[workspace 9 silent] flatpak run com.discordapp.Discord")
             hl.exec_cmd("[workspace 9 silent] ${pkgs.brave}/bin/brave --app=https://habitica.com --start-maximized")
